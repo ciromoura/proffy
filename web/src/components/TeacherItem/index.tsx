6d167886a5f90dccd9362b32a15ff53ff8164a10
Ciro Moura <ciro.dgm@gmail.com>
2020-08-05 19:24:58 -0300

    Dia 3 cont

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,32 +3,56 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher {
+    id: number;
+    cost: number;
+    name: string;
+    avatar: string;
+    whatsapp: string;
+    subject: string;
+    bio: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ( { teacher } ) => {
+
+    function createNewConnection() {
+        api.post('connections',{
+            user_id: teacher.id
+        })
+    }
+    
+    
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://pbs.twimg.com/profile_images/1019613231344734211/w_IiB8iD_400x400.jpg" alt="Ciro Moura" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Ciro Moura</strong>
-                    <span>Banco de Dados</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
             <p>
-                asdasdasdasd asd asd asd
-                <br />
-                <br />
-                asdadasdasdas
+                {teacher.bio}
             </p>
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button>
+                <a 
+                    target="_blank"
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,19 +1,32 @@
 import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
 
 import './styles.css';
+import api from '../../services/api';
 
 function TeacherList(){
+    const [teachers, setTeachers] = useState([]);
+
     const [subject, setSubject] = useState('');
     const [week_day, setWeekDay] = useState('');
     const [time, setTime] = useState('');
 
-    function searchTeachers(e: FormEvent){
+    async function searchTeachers(e: FormEvent){
         e.preventDefault();
+
+        const response = await api.get('classes', {
+            params: {
+                week_day,
+                subject,
+                time
+            }
+        });
+
+        setTeachers(response.data);
     }
 
     return (
@@ -65,11 +78,9 @@ function TeacherList(){
             </PageHeader>
 
             <main>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {teachers.map( (teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                })}
             </main>
         </div>
     );
